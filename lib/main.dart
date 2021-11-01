598f6bdb09e2da4b6461dcd195360fa0bd553c8a
Keben Carrillo <keben@icloud.com>
2021-11-01 13:38:24 -0700

    iOS/Android firebase set up

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,36 @@
-import "package:flutter/material.dart";
 import './app_screens/Screen1.dart';
+import "package:flutter/material.dart";
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(new CloudRaceApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(CloudRaceApp());
+}
 
 class CloudRaceApp extends StatelessWidget {
+  final Future<FirebaseApp> _fbApp = Firebase.initializeApp();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      //home: Screen1(),
+      //home: FutureBuilder(
+      //future: _fbApp,
+      //builder: (context, snapshot) {
+      //if (snapshot.hasError) {
+      //print ('You have an error: ${snapshot.error.toString()}');
+      //return Text('Something went wrong!');
+      //} else if (snapshot.hasData) {
+      //return MaterialApp(title: 'Cloud Race');
+      //} else {
+      //return Center(
+      //child: CircularProgressIndicator(),
+      //);
+      //}
+      //},
+      //));
+      //}
+//}
       home: Screen1(),
     );
   }
